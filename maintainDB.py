2dbb8c03f764fbd7a02b6c0d6459d042f0fe5778
smo957 <smo957@users.noreply.github.com>
2022-10-23 15:34:26 -0400

    check robots.txt

---
+++ maintainDB.py
@@ -0,0 +1,21 @@
+import urllib.robotparser
+
+rp = urllib.robotparser.RobotFileParser()
+useragent = '*'
+
+rp.set_url('https://www.sellerratings.com/robots.txt')
+rp.read()
+
+rrate = rp.request_rate(useragent)
+print(f'Request-rate details: {rrate}')
+if rrate:
+    print('Request-rate details')
+    print(f'Number of requests: {rrate.requests}')
+    print(f'Seconds: {rrate.seconds}')
+
+print(f'Crawl-delay: {rp.crawl_delay(useragent)}')
+
+if rp.can_fetch(useragent, 'https://www.sellerratings.com/amazon/usa'):
+    print("All good! Scrape away!")
+else:
+    print("Alert!! Can't scrape!")
